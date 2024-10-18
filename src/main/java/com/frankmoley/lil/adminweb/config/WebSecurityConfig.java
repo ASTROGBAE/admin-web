6e6fb83ab5d9c95df6c34ed0b579cdfc2dc0e8ab
Gabe Bretthauer <gabe.bretthauer@transport.nsw.gov.au>
2024-10-18 13:26:36 +1100

    jbdc implementation (sql messing up lol)

--- src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -1,16 +1,18 @@
 package com.frankmoley.lil.adminweb.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -26,15 +28,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        @SuppressWarnings("deprecation")
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    UserDetailsService users(DataSource dataSource) {
+        return new JdbcUserDetailsManager();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    static PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
