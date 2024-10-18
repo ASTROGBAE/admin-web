5818ee8f54904ac2ea91834f75302f3c1b17a141
Gabe Bretthauer <gabe.bretthauer@transport.nsw.gov.au>
2024-10-18 11:55:21 +1100

    In-memory authentication example (not currently working)

---
+++ src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.frankmoley.lil.adminweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(requests -> requests
+                        .antMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        @SuppressWarnings("deprecation")
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
